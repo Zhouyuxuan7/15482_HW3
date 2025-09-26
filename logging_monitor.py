0af911d805a8a2bf41702f297589ee5590398f7d
msomula1 <msomula@andrew.cmu.edu>
2025-09-26 14:09:20 -0400

    Updated logging

--- logging_monitor.py
+++ logging_monitor.py
@@ -1,4 +1,7 @@
 from monitor import *
+import logging
+import time 
+import os
 
 class LoggingMonitor(Monitor):
 
@@ -6,18 +9,35 @@ class LoggingMonitor(Monitor):
         super(LoggingMonitor, self).__init__("LoggingMonitor", period)
         # Put any iniitialization code here
         # BEGIN STUDENT CODE
+        log_file = "greenhouse.log"
+        if os.path.exists(log_file):
+            open(log_file, 'w').close()
+        logging.basicConfig(filename="greenhouse.log", level=logging.INFO, format="%(message)s")
+        header = "time, light, temp, humid, smoist, weight,level, fan, wpump, led, camera"
+        logging.info(header)
         # END STUDENT CODE
 
     def perceive(self):
         # BEGIN STUDENT CODE
-        # END STUDENT CODE
         pass
+        # END STUDENT CODE
+        
 
     def monitor(self):
         # Use self.sensorData and self.actuator_state to log the sensor and
         #  actuator data, preferably as a comma-separated line of values.
         #  Make sure to timestamp the line of data
         # BEGIN STUDENT CODE
+        timestamp = time.time()  # seconds since epoch
+        # Build CSV row
+        sensor_values = [str(self.sensordata.get(k, "")) for k in [
+            "light", "temp", "humid", "smoist", "weight", "level"
+        ]]
+        actuator_values = [str(self.actuator_state.get(k, "")) for k in [
+            "fan", "wpump", "led", "camera"
+        ]]
+        row = ",".join([str(timestamp)] + sensor_values + actuator_values)
+        logging.info(row)
         # END STUDENT CODE
         pass
 
